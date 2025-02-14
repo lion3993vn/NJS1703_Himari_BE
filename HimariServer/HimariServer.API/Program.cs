8832fbeb731723d9c6a88a077f5ca530b5b9a882
Tran Quang Huy (K17 HCM) <91615324+huytqse170597@users.noreply.github.com>
2025-02-14 22:35:16 +0700

    fix docker file cicd chay

--- HimariServer/HimariServer.API/Program.cs
+++ HimariServer/HimariServer.API/Program.cs
@@ -1,3 +1,4 @@
+using HimariServer.API;
 using HimariServer.API.Middlewares;
 using HimariServer.Repository.DBContext;
 using HimariServer.Service.BusinessModels.ResultModels;
@@ -10,52 +11,46 @@ using Microsoft.OpenApi.Models;
 using System.Text;
 using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
 
-namespace HimariServer.API
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers().ConfigureApiBehaviorOptions(options =>
 {
-    public class Program
+    options.InvalidModelStateResponseFactory = context =>
     {
-        public static void Main(string[] args)
+        var errors = context.ModelState
+            .Where(m => m.Value.Errors.Count > 0)
+            .SelectMany(m => m.Value.Errors)
+            .Select(e => e.ErrorMessage)
+            .ToList();
+
+        var response = new BaseResponseModel
         {
-            var builder = WebApplication.CreateBuilder(args);
+            StatusCode = StatusCodes.Status400BadRequest,
+            Message = string.Join("; ", errors)
+        };
 
-            // Add services to the container.
+        return new BadRequestObjectResult(response);
+    };
+});
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Himari Server", Version = "v.1.0" });
 
-            builder.Services.AddControllers().ConfigureApiBehaviorOptions(options =>
-            {
-                options.InvalidModelStateResponseFactory = context =>
-                {
-                    var errors = context.ModelState
-                        .Where(m => m.Value.Errors.Count > 0)
-                        .SelectMany(m => m.Value.Errors)
-                        .Select(e => e.ErrorMessage)
-                        .ToList();
-
-                    var response = new BaseResponseModel
-                    {
-                        StatusCode = StatusCodes.Status400BadRequest,
-                        Message = string.Join("; ", errors)
-                    };
-
-                    return new BadRequestObjectResult(response);
-                };
-            });
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Himari Server", Version = "v.1.0" });
-
-                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-                {
-                    In = ParameterLocation.Header,
-                    Description = "Please enter a valid token",
-                    Name = "Authorization",
-                    Type = SecuritySchemeType.Http,
-                    BearerFormat = "JWT",
-                    Scheme = "Bearer"
-                });
-                c.AddSecurityRequirement(new OpenApiSecurityRequirement
-                {
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Please enter a valid token",
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        BearerFormat = "JWT",
+        Scheme = "Bearer"
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
                     {
                         new OpenApiSecurityScheme
                         {
@@ -67,96 +62,93 @@ namespace HimariServer.API
                         },
                         new string[]{}
                     }
-                });
-            });
+    });
+});
 
-            builder.Services.AddCors(options =>
-            {
-                options.AddPolicy("app-cors",
-                    builder =>
-                    {
-                        builder.AllowAnyOrigin()
-                        .AllowAnyHeader()
-                        .WithExposedHeaders("X-Pagination")
-                        .AllowAnyMethod();
-                    });
-            });
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("app-cors",
+        builder =>
+        {
+            builder.AllowAnyOrigin()
+            .AllowAnyHeader()
+            .WithExposedHeaders("X-Pagination")
+            .AllowAnyMethod();
+        });
+});
 
-            builder.Services.AddAuthentication(options =>
-            {
-                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
-            }).AddJwtBearer(options =>
-            {
-                options.SaveToken = true;
-                options.RequireHttpsMetadata = false;
-                options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
-                {
-                    ValidateIssuer = true,
-                    ValidateAudience = true,
-                    ValidAudience = builder.Configuration["JWT:ValidAudience"],
-                    ValidIssuer = builder.Configuration["JWT:ValidIssuer"],
-                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:SecretKey"]))
-                };
-            });
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.SaveToken = true;
+    options.RequireHttpsMetadata = false;
+    options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidAudience = builder.Configuration["JWT:ValidAudience"],
+        ValidIssuer = builder.Configuration["JWT:ValidIssuer"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:SecretKey"]))
+    };
+});
 
-            // ===================== CONFIG DATABASE CONNECTION =======================
+// ===================== CONFIG DATABASE CONNECTION =======================
 
-            var connection = string.Empty;
-            if (builder.Environment.IsDevelopment())
-            {
-                connection = builder.Configuration.GetConnectionString("HimariServerLocal");
-            }
-            else
-            {
-                connection = builder.Configuration.GetConnectionString("HimariServerLocal");
-            }
-
-            builder.Services.AddDbContext<HimariServerContext>(options =>
-            {
-                options.UseSqlServer(connection);
-            });
-
-            // ==========================================================
-
-            builder.Services.AddAutoMapper(typeof(MapperConfigProfile).Assembly);
-
-            builder.Services.AddInfractstructure(builder.Configuration);
-
-            builder.Services.AddStackExchangeRedisCache(options =>
-            {
-                options.Configuration = builder.Configuration.GetSection("RedisSettings:RedisConnectionString").Value;
-                options.InstanceName = builder.Configuration.GetSection("RedisSettings:InstanceName").Value;
-                options.ConfigurationOptions = new StackExchange.Redis.ConfigurationOptions()
-                {
-                    AbortOnConnectFail = true,
-                    EndPoints = { options.Configuration }
-                };
-            });
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            app.UseSwagger();
-            app.UseSwaggerUI(c =>
-            {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Himari Server v.01");
-            });
-
-            app.UseHttpsRedirection();
-
-            app.UseCors("app-cors");
-
-            app.UseAuthentication();
-
-            app.UseAuthorization();
-
-            app.MapControllers();
-
-            app.UseMiddleware<ExceptionHandlingMiddleware>();
-
-            app.Run();
-        }
-    }
+var connection = string.Empty;
+if (builder.Environment.IsDevelopment())
+{
+    connection = builder.Configuration.GetConnectionString("HimariServerLocal");
 }
+else
+{
+    connection = builder.Configuration.GetConnectionString("HimariServerLocal");
+}
+
+builder.Services.AddDbContext<HimariServerContext>(options =>
+{
+    options.UseSqlServer(connection);
+});
+
+// ==========================================================
+
+builder.Services.AddAutoMapper(typeof(MapperConfigProfile).Assembly);
+
+builder.Services.AddInfractstructure(builder.Configuration);
+
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = builder.Configuration.GetSection("RedisSettings:RedisConnectionString").Value;
+    options.InstanceName = builder.Configuration.GetSection("RedisSettings:InstanceName").Value;
+    options.ConfigurationOptions = new StackExchange.Redis.ConfigurationOptions()
+    {
+        AbortOnConnectFail = true,
+        EndPoints = { options.Configuration }
+    };
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Himari Server v.01");
+});
+
+app.UseHttpsRedirection();
+
+app.UseCors("app-cors");
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.UseMiddleware<ExceptionHandlingMiddleware>();
+
+app.Run();
