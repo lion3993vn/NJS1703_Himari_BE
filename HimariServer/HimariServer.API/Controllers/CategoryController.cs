d9c08bbc493072e31c6f60a8a3efdb92fe015372
lion3993vn <lion3993vn@gmail.com>
2025-02-15 20:53:55 +0700

    add: pagination category

---
+++ HimariServer/HimariServer.API/Controllers/CategoryController.cs
@@ -0,0 +1,24 @@
+﻿using HimariServer.Repository.Commons;
+using HimariServer.Service.Services.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HimariServer.API.Controllers
+{
+    [Route("api/v1/categories")]
+    [ApiController]
+    public class CategoryController : BaseController
+    {
+        private readonly ICategoryService _categoryService;
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet]
+        public Task<IActionResult> GetCategories(PaginationParameter paginationParameter)
+        {
+            return ValidateAndExecute(async () => await _categoryService.GetCategoriesPaginationAsync(paginationParameter));
+        }
+    }
+}
