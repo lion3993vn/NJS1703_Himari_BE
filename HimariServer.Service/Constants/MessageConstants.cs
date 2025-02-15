15ee0cf482f7b52b18b9ddbb6bcbef20745a2167
Tran Quang Huy (K17 HCM) <91615324+huytqse170597@users.noreply.github.com>
2025-02-16 00:15:05 +0700

    add: get all product, get product by id, update product, soft delete product

--- HimariServer.Service/Constants/MessageConstants.cs
+++ HimariServer.Service/Constants/MessageConstants.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace HimariServer.Service.Constants
-{
+{ 
     public class MessageConstants
     {
         public const string GET_USER_BY_EMAIL_SUCCESS = "Get user by email successfully";
@@ -13,6 +13,12 @@ namespace HimariServer.Service.Constants
 
         public const string GET_LIST_CATEGORY_SUCCESS = "Get list category successfully";
         public const string CATEGORY_NOT_FOUND = "Category not found";
+
+        public const string GET_LIST_PRODUCT_SUCCESS = "Get list product successfully";
+        public const string PRODUCT_FOUND = "Product not founds";
+        public const string PRODUCT_NOT_FOUND = "Product not found";
+        public const string PRODUCT_DELETE_SUCCESS = "Product deleted successfully";
+        public const string PRODUCT_UPDATE_SUCCESS = "Product updated successfully";
 
         public const string TOKEN_NOT_VALID = "TokenNotValid";
         public const string LOGIN_SUCCESS_MESSAGE = "Login successfully";
