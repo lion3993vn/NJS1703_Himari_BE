c5d1b8c019fca36c0c41ffdf81c53d9b0679c55f
lion3993vn <lion3993vn@gmail.com>
2025-02-16 21:49:38 +0700

    add: crud category (not finish)

--- HimariServer.Service/BusinessModels/CategoryModels/CategoryUpdateModel.cs
+++ HimariServer.Service/BusinessModels/CategoryModels/CategoryUpdateModel.cs
@@ -6,16 +6,15 @@ using System.Threading.Tasks;
 
 namespace HimariServer.Service.BusinessModels.CategoryModels
 {
-    public class CategoryModels
+    public class CategoryUpdateModel
     {
+        public int id { get; set; }
+
         public string CategoryName { get; set; }
 
         public string Description { get; set; }
 
         public int? ParentCategoryId { get; set; }
 
-        public string? ParentCategoryName { get; set; }
-
-        public string Status { get; set; }
     }
 }
