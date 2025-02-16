79d33cb8668bfb5f0161e7f14940276cf484c480
Tuongtan <tuongtan110203@gmail.com>
2025-02-16 20:12:49 +0700

    fix program.cs

--- HimariServer.API/Program.cs
+++ HimariServer.API/Program.cs
@@ -37,11 +37,11 @@ builder.Services.AddControllers().ConfigureApiBehaviorOptions(options =>
 
 // AddJsonOptions
 
-builder.Services.AddControllers()
-    .AddJsonOptions(options =>
-    {
-        options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
-    });
+//builder.Services.AddControllers()
+//    .AddJsonOptions(options =>
+//    {
+//        options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
+//    });
 
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
