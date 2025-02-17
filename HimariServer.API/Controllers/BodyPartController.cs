2152a7c37a17e41c118d49958ee12baa99a7671f
NET_12_PhamVanTuanHieu <hieupvtse171097@fpt.edu.vn>
2025-02-17 09:20:51 +0700

    add: body part config

---
+++ HimariServer.API/Controllers/BodyPartController.cs
@@ -0,0 +1,19 @@
+﻿using HimariServer.Service.Services.Implements;
+using HimariServer.Service.Services.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HimariServer.API.Controllers
+{
+    [Route("api/v1/body-parts")]
+    [ApiController]
+    public class BodyPartController : BaseController
+    {
+        private readonly IBodyPartService _bodyPartService;
+
+        public BodyPartController(IBodyPartService bodyPartService)
+        {
+            _bodyPartService = bodyPartService;
+        }
+    }
+}
