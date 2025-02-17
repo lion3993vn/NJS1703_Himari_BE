419e426e6494cd02b39aef1d891e77bbfd8ca5db
Tran Quang Huy (K17 HCM) <91615324+huytqse170597@users.noreply.github.com>
2025-02-17 20:11:11 +0700

    add: create product, fix check null category id in update product

---
+++ HimariServer.Service/BusinessModels/ProductModels/CreateProductModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HimariServer.Service.BusinessModels.ProductModels
+{
+    public class CreateProductModel
+    {
+        public required string ProductName { get; set; }
+
+        public required string Description { get; set; }
+
+        public int? Price { get; set; }
+
+        public int? Quantity { get; set; }
+
+        public required string ImageUrl { get; set; }
+
+        public required string Status { get; set; }
+
+        public int? CategoryId { get; set; }
+        public int? BrandId { get; set; }
+
+        public bool? Gender { get; set; }
+    }
+}
