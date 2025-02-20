bbe6db794ca9cb3a1dbbba384a0a1f147445b0f6
NET_12_PhamVanTuanHieu <hieupvtse171097@fpt.edu.vn>
2025-02-20 09:36:25 +0700

    add: firebase utils

---
+++ HimariServer.Service/Utils/FirebaseLibrary.cs
@@ -0,0 +1,78 @@
+﻿using FirebaseAdmin.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HimariServer.Service.Utils
+{
+    internal class FirebaseLibrary
+    {
+        public static async Task<string> SendMessageFireBase(string title, string body, string token)
+        {
+            try
+            {
+                var message = new Message()
+                {
+                    Notification = new Notification()
+                    {
+                        Title = title,
+                        Body = body,
+                        ImageUrl = "https://firebasestorage.googleapis.com/v0/b/thelavenstore-fe036.appspot.com/o/HimariLogo.jpg?alt=media&token=33c99a89-4a91-4c9f-9b81-2aea896ed569"
+                    },
+                    Token = token
+                };
+
+                var reponse = await FirebaseMessaging.DefaultInstance.SendAsync(message);
+                return reponse;
+            }
+            catch
+            {
+                return "";
+            }
+        }
+
+        public static async Task<bool> SendRangeMessageFireBase(string title, string body, List<string> tokens)
+        {
+            var message = new MulticastMessage()
+            {
+                Notification = new Notification()
+                {
+                    Title = title,
+                    Body = body,
+                    ImageUrl = "https://firebasestorage.googleapis.com/v0/b/thelavenstore-fe036.appspot.com/o/HimariLogo.jpg?alt=media&token=33c99a89-4a91-4c9f-9b81-2aea896ed569"
+                },
+                Tokens = tokens
+            };
+
+            var reponse = await FirebaseMessaging.DefaultInstance.SendEachForMulticastAsync(message);
+            return true;
+
+        }
+
+        public static async Task<string> SendMessagePaymentFireBase(string title, string body, string token)
+        {
+            try
+            {
+                var message = new Message()
+                {
+                    Notification = new Notification()
+                    {
+                        Title = title,
+                        Body = body,
+                        ImageUrl = "https://firebasestorage.googleapis.com/v0/b/thelavenstore-fe036.appspot.com/o/HimariLogo.jpg?alt=media&token=33c99a89-4a91-4c9f-9b81-2aea896ed569"
+                    },
+                    Token = token
+                };
+
+                var reponse = await FirebaseMessaging.DefaultInstance.SendAsync(message);
+                return reponse;
+            }
+            catch
+            {
+                return "";
+            }
+        }
+    }
+}
