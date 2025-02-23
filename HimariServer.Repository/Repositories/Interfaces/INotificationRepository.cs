7d34cd8c61044203ef255dbc8162a2d9e597ec3e
NET_12_PhamVanTuanHieu <hieupvtse171097@fpt.edu.vn>
2025-02-23 09:05:34 +0700

    add: notifcation

---
+++ HimariServer.Repository/Repositories/Interfaces/INotificationRepository.cs
@@ -0,0 +1,14 @@
+﻿using HimariServer.Repository.Entities;
+using HimariServer.Repository.Repositories.Generic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HimariServer.Repository.Repositories.Interfaces
+{
+    public interface INotificationRepository : IGenericRepository<Notification>
+    {
+    }
+}
---
+++ HimariServer.Service/Services/Implements/NotificationService.cs
@@ -0,0 +1,44 @@
+﻿using AutoMapper;
+using HimariServer.Repository.UnitOfWork;
+using HimariServer.Service.BusinessModels.ResultModels;
+using HimariServer.Service.Constants;
+using HimariServer.Service.Exceptions;
+using HimariServer.Service.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HimariServer.Service.Services.Implements
+{
+    public class NotificationService : INotificationService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public NotificationService(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+        public Task<BaseResponseModel> GetNotificationById(int id)
+        {
+            var noti = await _unitOfWork.NotificationRepository.GetByIdAsync(id);
+            if (noti == null)
+            {
+                throw new NotExistException("", MessageConstants.NOTI_NOT_EXIST);
+            }
+        }
+
+        public Task<bool> PushListMessageFirebase(string title, string body, List<string> fcmTokens)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> PushMessageFirebase(string title, string body, int userId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
