40f965bccf1bb71219b6261fecb41752cb0199a8
Tran Quang Huy (K17 HCM) <91615324+huytqse170597@users.noreply.github.com>
2025-03-01 22:15:34 +0700

    update api delete category

--- HimariServer.Repository/Repositories/Implements/ProductRepository.cs
+++ HimariServer.Repository/Repositories/Implements/ProductRepository.cs
@@ -2,6 +2,7 @@
 using HimariServer.Repository.Entities;
 using HimariServer.Repository.Repositories.Generic;
 using HimariServer.Repository.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace HimariServer.Repository.Repositories.Implements
 {
@@ -12,6 +13,11 @@ namespace HimariServer.Repository.Repositories.Implements
         public ProductRepository(HimariServerContext context) : base(context)
         {
             _context = context;
+        }
+
+        public async Task<bool> IsContainProduct(int categoryId)
+        {
+            return await _context.Products.AnyAsync(x => x.CategoryId == categoryId && !x.IsDeleted);
         }
     }
 }
