5548cb633fc1c24dd03a8c375195d72b346c1bac
Tuongtan110203 <tuongtan110203@gmail.com>
2025-03-10 20:08:36 +0700

    fix: user address

--- HimariServer.Service/BusinessModels/UserModels/UpdateUserModel.cs
+++ HimariServer.Service/BusinessModels/UserModels/UpdateUserModel.cs
@@ -12,7 +12,10 @@ namespace HimariServer.Service.BusinessModels.UserModels
         public required string Email { get; set; }
         public string? FullName { get; set; }
         public string? PhoneNumber { get; set; }
-        public string? Address { get; set; }
+        public string Province { get; set; }
+        public string District { get; set; }
+        public string Ward { get; set; }
+        public string AddressBonus { get; set; }
         public string? AvatarUrl { get; set; }
     }
 }
