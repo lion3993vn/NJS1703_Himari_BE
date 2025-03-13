bdac328ea13699de7f73eb61f923a5239a8a7dcf
Tuongtan110203 <tuongtan110203@gmail.com>
2025-03-13 22:06:17 +0700

    add: email content

---
+++ HimariServer.API/Controllers/EmailController.cs
@@ -0,0 +1,34 @@
+﻿using HimariServer.Service.BusinessModels.EmailModels;
+using HimariServer.Service.Services.Interfaces;
+using HimariServer.Service.Utils;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace HimariServer.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmailController : ControllerBase
+    {
+        private readonly IMailService _mailService;
+
+        public EmailController(IMailService mailService)
+        {
+            _mailService = mailService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            await _mailService.SendEmailAsync(new MailRequest
+            {
+                ToEmail = "lion3993vn@gmail.com",
+                Subject = "Chào mừng bạn đến với Himari!",
+                Body = EmailUtils.WelcomeEmail("Hiếu"),
+                Attachments = null
+            });
+            return Ok("Himari Email Service");
+        }
+    }
+}
