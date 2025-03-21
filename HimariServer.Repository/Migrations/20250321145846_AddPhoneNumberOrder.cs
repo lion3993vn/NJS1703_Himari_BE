f88a5b93025b500c555bc1817c3251e18ebf20fc
Tran Quang Huy (K17 HCM) <91615324+huytqse170597@users.noreply.github.com>
2025-03-21 22:16:43 +0700

    add phonenumber for order

---
+++ HimariServer.Repository/Migrations/20250321145846_AddPhoneNumberOrder.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HimariServer.Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPhoneNumberOrder : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PhoneNumber",
+                table: "Order",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PhoneNumber",
+                table: "Order");
+        }
+    }
+}
