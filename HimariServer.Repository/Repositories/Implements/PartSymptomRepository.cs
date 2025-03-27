b6faac92a6d446e4c83ba517ebc49181a49adcca
Tran Quang Huy (K17 HCM) <91615324+huytqse170597@users.noreply.github.com>
2025-03-27 10:38:29 +0700

    update delete bodypart

--- HimariServer.Repository/Repositories/Implements/PartSymptomRepository.cs
+++ HimariServer.Repository/Repositories/Implements/PartSymptomRepository.cs
@@ -2,6 +2,7 @@ using HimariServer.Repository.DBContext;
 using HimariServer.Repository.Entities;
 using HimariServer.Repository.Repositories.Generic;
 using HimariServer.Repository.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace HimariServer.Repository.Repositories.Implements
 {
@@ -12,6 +13,10 @@ namespace HimariServer.Repository.Repositories.Implements
         public PartSymptomRepository(HimariServerContext context) : base(context)
         {
             _context = context;
+        }
+        public async Task<bool> IsContainPartSymptom(int bodyPartId)
+        {
+            return await _context.PartSymptoms.AnyAsync(x => x.BodyPartId == bodyPartId && !x.IsDeleted);
         }
     }
 }
